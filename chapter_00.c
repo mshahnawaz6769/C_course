01af6ec5f0411ce22de244b4ede1c8ab45c5d72f
mshahnawaz6769 <mshahnawaz6769@gmai.com>
2026-01-16 16:10:58 +0500

    initiate

---
+++ chapter_00.c
@@ -0,0 +1,8 @@
+// CHAPTER 00: INTRODUCTION AND DOWNLOADING PROCESS
+
+// -> Stake ovew flow (Top 10 language)
+// -> Almost in every year (Top  03)
+// -> It's the mmother o all programming language
+// -> "DENNIS - RITCHIE" discovered 'C' in 1972
+// -> It's used in mbedded systems, Real time, Algorithms, operating system development and AI era.
+
